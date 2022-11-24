2462a840be78df84a2eef7b89829314a66e9c0d1
Sho Mizutani <lowply@github.com>
2022-11-24 22:10:16 +0900

    Update .gitignore and add sync.sh

---
+++ script/sync.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#/
+#/ Usage:
+#/
+#/ sync.sh [TAG]
+#/
+
+usage() {
+    grep '^#/' < ${0} | cut -c4-
+    exit 1
+}
+
+[ $# -eq 1 ] || usage
+
+TAG=${1}
+
+[ -d git.git ] || git clone --bare https://github.com/git/git.git
+[ -d contrib ] && rm -rf contrib
+
+GIT_DIR=git.git GIT_WORK_TREE=. git checkout ${TAG} -- contrib
+git add contrib
+git commit -m "Import ${TAG}"
+git push
+git tag -a "${TAG}" -m "Mirror of the contrib directory of Git ${TAG}"
+git push origin --tags
