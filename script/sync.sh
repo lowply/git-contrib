79e0c05e6e5c1fb5b42deec576a63285e62e37a1
Sho Mizutani <lowply@github.com>
2022-11-24 22:19:34 +0900

    Update sync.sh

--- script/sync.sh
+++ script/sync.sh
@@ -18,7 +18,7 @@ TAG=${1}
 [ -d git.git ] || git clone --bare https://github.com/git/git.git
 [ -d contrib ] && rm -rf contrib
 
-GIT_DIR=git.git GIT_WORK_TREE=. git checkout ${TAG} -- contrib
+GIT_DIR=git.git GIT_WORK_TREE=. git checkout ${TAG} -- contrib || exit $?
 git add contrib
 git commit -m "Import ${TAG}"
 git push
